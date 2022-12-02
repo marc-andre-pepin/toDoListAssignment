4be290a1f6e5c0798ee3991ca5f1f20c47add0a9
marc-andre-pepin <109531811+marc-andre-pepin@users.noreply.github.com>
2022-12-01 18:20:34 -0600

    Created three files for project

---
+++ index.js
@@ -0,0 +1,120 @@
+// index.js
+console.log("js is working today")
+
+$(document).ready(function(){
+    var displayTask = function (task) {
+        $('#todo-list').append('<div class="row"><p class="col-xs-8">' + task.content + '</p><button class="delete" data-id="' + task.id + '">Delete</button><input type="checkbox" class="mark-complete" data-id="' + task.id + '"' + (task.completed ? 'checked' : '') + '>');
+    }
+
+    $('#active-filter').change(function (event) {
+        var completedTaskRows = $('input.mark-complete:checked').closest('div.row');
+        if (this.checked) {
+            completedTaskRows.hide();
+        } else {
+            completedTaskRows.show();
+        }
+    })
+
+    var getAndDisplayAllTasks = function () {
+        $.ajax({
+            type: 'GET',
+            url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=47',
+            dataType: 'json',
+            success: function (response, textStatus) {
+                $('#todo-list').empty();
+                response.tasks.forEach(displayTask);
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
+        });
+    }
+
+    var createTask = function () {
+        $.ajax({
+            type: 'POST',
+            url: 'https://fewd-todolist-api.onrender.com/tasks?api_key=47',
+            contentType: 'application/json',
+            dataType: 'json',
+            data: JSON.stringify({
+                task: {
+                    content: $('#new-task-content').val()
+                }
+            }),
+            success: function (response, textStatus) {
+                $('#new-task-content').val('');
+                displayTask(response.task)
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
+        });
+    }
+
+    $('#create-task').on('submit', function (e) {
+        e.preventDefault();
+        createTask();
+    });
+
+    var deleteTask = function (id) {
+        $.ajax({
+            type: 'DELETE',
+            url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '?api_key=47',
+            success: function (response, textStatus) {
+                getAndDisplayAllTasks();
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
+        });
+    }
+
+    $(document).on('click', '.delete', function () {
+        deleteTask($(this).data('id'));
+    });
+
+    var markTaskComplete = function (id) {
+        $.ajax({
+            type: 'PUT',
+            url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_complete?api_key=47',
+            dataType: 'json',
+            success: function (response, textStatus) {
+                getAndDisplayAllTasks();
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
+        });
+    }
+
+    $(document).on('change', '.mark-complete', function () {
+        if (this.checked) {
+            markTaskComplete($(this).data('id'));
+        }
+    });
+
+    var markTaskActive = function (id) {
+        $.ajax({
+            type: 'PUT',
+            url: 'https://fewd-todolist-api.onrender.com/tasks/' + id + '/mark_active?api_key=47',
+            dataType: 'json',
+            success: function (response, textStatus) {
+                getAndDisplayAllTasks();
+            },
+            error: function (request, textStatus, errorMessage) {
+                console.log(errorMessage);
+            }
+        });
+    }
+
+    $(document).on('change', '.mark-complete', function () {
+        if (this.checked) {
+            markTaskComplete($(this).data('id'));
+        } else {
+            markTaskActive($(this).data('id'));
+        }
+    });
+
+    getAndDisplayAllTasks();
+
+});
